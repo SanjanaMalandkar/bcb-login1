40c7345c818ca4e1acfce3406f7cd1d56b6cf717
SanjanaMalandkar <40349894+SanjanaMalandkar@users.noreply.github.com>
2018-07-03 12:20:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://barcampbangalore.com/bcb/wp-content/uploads/2018/01/BCB-LOGO-open-graph.jpg" className="App-logo" alt="logo" />
+          <h1 className="App-title">Login/Sign Up</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
